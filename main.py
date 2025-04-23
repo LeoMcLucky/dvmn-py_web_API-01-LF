80d70bf6585efbf831d20bdce4ae10223e990faa
Lev <47698500+LeoMcLucky@users.noreply.github.com>
2025-04-23 16:45:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def main():
+    url_template = "https://wttr.in/{}?MmnqT&lang=ru"
+    locations = [
+        'London',
+        'svo',
+        'Череповец'
+    ]
+    for location in locations:
+        url = url_template.format(location)
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+
+
+if __name__ == '__main__':
+    main()
